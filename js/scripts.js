23f55f10ec27242ce7adeae1a316573c18b92bb2
Alicja <alanotowska@yahoo.co.uk>
2023-05-17 14:28:43 +0200

    adds bootstrap utility classes to ul and li elements

--- js/scripts.js
+++ js/scripts.js
@@ -21,6 +21,7 @@ let pokemonRepository = (function () {
         addListItem: function(pokemon) {
             let pokemonList = document.querySelector('.pokemon-list');
             let listItem = document.createElement('li');
+            listItem.classList.add('list-group-item');
             let button = document.createElement('button');
             button.innerText = (pokemon.name);
             button.classList.add('pokemonButton');
